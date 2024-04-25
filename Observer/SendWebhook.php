108144843f37a3a6a54378076b9a145a1d5527e3
bram de leeuw <bramdeleeuw@gmail.com>
2024-04-25 12:32:04 +0200

    Stuur de order direct mee in de observer. Dit scheelt een api-call vanaf webshoplocatie.

--- Observer/SendWebhook.php
+++ Observer/SendWebhook.php
@@ -3,21 +3,25 @@
 namespace Webshoplocatie\Integration\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Api\ExtensibleDataObjectConverter;
 
 class SendWebhook implements ObserverInterface
 {
     protected $_scopeConfig;
     protected $_curl;
     protected $_logger;
+    protected $_dataObjectConverter;
 
     public function __construct(
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Framework\HTTP\Client\Curl $curl,
-        \Psr\Log\LoggerInterface $logger
+        \Psr\Log\LoggerInterface $logger,
+        ExtensibleDataObjectConverter $dataObjectConverter
     ) {
         $this->_scopeConfig = $scopeConfig;
         $this->_curl = $curl;
         $this->_logger = $logger;
+        $this->_dataObjectConverter = $dataObjectConverter;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
@@ -43,6 +47,7 @@ class SendWebhook implements ObserverInterface
         $orderData = [];
         $orderData['increment_id'] = $orderId;
         $orderData['signature'] = $signature;
+        $orderData['order'] = $this->_dataObjectConverter->toNestedArray($order, [], \Magento\Sales\Api\Data\OrderInterface::class);
 
         $this->_curl->setHeaders([
             'Content-Type' => 'application/json'
